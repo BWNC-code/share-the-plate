19ba630ece171849f528bf47868b9fcfb2e4a5d7
BWNC-code <bryn.wheeler@gmail.com>
2023-07-21 14:49:28 +0000

    add initial planned models

--- share_the_plate/models.py
+++ share_the_plate/models.py
@@ -1,3 +1,78 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.utils.text import slugify
+from cloudinary.models import CloudinaryField
 
 # Create your models here.
+
+STATUS = ((0, "Draft"), (1, "Published"))
+
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(blank=True)
+    profile_picture = models.CharField(max_length=255, default='default.jpg')
+
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255, unique=True)
+    slug = models.SlugField(unique=True)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    cooking_time = models.IntegerField()
+    difficulty_level = models.CharField(max_length=255)
+    featured_image = CloudinaryField('image', default='placeholder')
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def save(self, *args, **kwargs):
+        if not self.slug:
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return self.title
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class RecipeCategory(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+
+class RecipeTag(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    tag = models.ForeignKey(Tag, on_delete=models.CASCADE)
+
+
+class Comment(models.Model):
+    comment_text = models.TextField()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['-created_at']
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
+
+
+class Like(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        unique_together = ('user', 'recipe')
