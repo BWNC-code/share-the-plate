d0ccdedac6498fd9890b90a335bdaabaf7773f20
BWNC-code <bryn.wheeler@gmail.com>
2023-07-21 15:40:37 +0000

    add login view from django standard

--- share_the_plate/views.py
+++ share_the_plate/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.contrib.auth.views import LoginView
 
 
 # Create your views here.
@@ -15,3 +16,8 @@ def recipe_list(request):
     }
 
     return render(request, 'share_the_plate/recipe_list.html', context)
+
+
+class CustomLoginView(LoginView):
+    template_name = 'share_the_plate/login.html'
+    redirect_authenticated_user = True
