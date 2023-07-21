575bda33b4fddd001c2513dd766b1a614f79febf
BWNC-code <bryn.wheeler@gmail.com>
2023-07-21 14:48:05 +0000

    add recipe_list view

--- share_the_plate/views.py
+++ share_the_plate/views.py
@@ -4,3 +4,14 @@ from django.shortcuts import render
 # Create your views here.
 def home_page(request):
     return render(request, 'share_the_plate/index.html')
+
+
+def recipe_list(request):
+    # Get the 10 most recent recipes
+    recipes = Recipe.objects.order_by('-created_at')[:10]
+
+    context = {
+        'recipes': recipes
+    }
+
+    return render(request, 'share_the_plate/recipe_list.html', context)
