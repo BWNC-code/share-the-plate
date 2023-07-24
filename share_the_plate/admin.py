15d924fb205d969ef9f7ee69640e98f31a1c31bc
BWNC-code <bryn.wheeler@gmail.com>
2023-07-24 16:59:24 +0000

    add recipes and comments to admin page view

--- share_the_plate/admin.py
+++ share_the_plate/admin.py
@@ -1,3 +1,26 @@
 from django.contrib import admin
+from .models import Recipe, Category, Comment
+from django_summernote.admin import SummernoteModelAdmin
 
 # Register your models here.
+
+
+@admin.register(Recipe)
+class RecipeAdmin(SummernoteModelAdmin):
+
+    prepopulated_fields = {'slug': ('title',)}
+    list_filter = ('status', 'created_at')
+    list_display = ('title', 'slug', 'status', 'created_at')
+    search_fields = ['title', 'ingredients']
+    summernote_fields = ('ingredients', 'instructions')
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+
+    list_display = ('body', 'user', 'created_at', 'recipe')
+    list_filter = ('created_at', 'user')
+    search_fields = ['body', 'email', 'user']
+
+    name = Comment.user
+
