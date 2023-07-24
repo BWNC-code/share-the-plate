7eb5c80eb7605707a73fd6ca2053cc875152b23f
BWNC-code <bryn.wheeler@gmail.com>
2023-07-24 20:28:51 +0000

    split user profile page into separate templates

--- share_the_plate/urls.py
+++ share_the_plate/urls.py
@@ -10,5 +10,7 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'),
     path('recipes/', views.recipe_list, name='recipe_list'),
     path('recipe/<slug:slug>/', views.recipe_detail, name='recipe_detail'),
-    path('profile/<str:username>/', views.profile, name='profile')
+    path('profile/<str:username>/', views.profile_info, name='profile_info'),
+    path('profile/<str:username>/recipes/', views.user_recipes, name='user_recipes'),
+    path('profile/<str:username>/likes/', views.liked_recipes, name='liked_recipes'),
 ]
