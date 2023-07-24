1d8abd46cfcbc924f2ffe9327427fdcd03dbdeed
BWNC-code <bryn.wheeler@gmail.com>
2023-07-24 17:02:44 +0000

    add profile page url

--- share_the_plate/urls.py
+++ share_the_plate/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('', views.home_page, name='index'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('signup/', views.signup, name='signup'),
-    path('recipes/', views.recipe_list, name='recipe_list')
+    path('recipes/', views.recipe_list, name='recipe_list'),
+    path('profile/<str:username>/', views.profile, name='profile')
 ]
