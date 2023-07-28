19903c5d3b151bf9b068c2b5453578ba55250d58
BWNC-code <bryn.wheeler@gmail.com>
2023-07-28 18:31:53 +0000

    link debug value to env variable

--- django_share_the_plate/settings.py
+++ django_share_the_plate/settings.py
@@ -33,7 +33,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.environ.get("DEBUG") == "True"
 
 ALLOWED_HOSTS = [
     '8000-bwnccode-sharetheplate-ktokfwe9u2x.ws-eu101.gitpod.io',
